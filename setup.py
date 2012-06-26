d1047174ff8e75f6388f57b27cb4f820fa33230a
Daniele Procida <daniele@vurt.org>
2012-06-26 10:33:30 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='Arkestra publications',
+    version='0.0.2',
+    author='Ben Rowett and Daniele Procida',
+    author_email='daniele@vurt.org',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe = False,
+    license='LICENCE.txt',
+    description='Symplectic Publications integration & output for Arkestra',
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+)      
