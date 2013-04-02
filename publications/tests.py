ae58629213d2c89fc4e9f091b428c7275aa87d67
Daniele Procida <daniele@vurt.org>
2013-04-02 14:31:33 +0100

    test for 7b9c5714644d109524cddb8e403c3ea119ad7650

--- publications/tests.py
+++ publications/tests.py
@@ -6,18 +6,41 @@ from django.utils.encoding import force_unicode
 
 import unittest
 
-from publications.models import Researcher
-from contacts_and_people.models import Person
+from cms.models.placeholdermodel import Placeholder
+from cms.api import add_plugin
+
+from publications.models import Researcher, PublicationsPlugin
+from contacts_and_people.models import Person, Entity
 
 class ResearcherTests(unittest.TestCase):
     def test_methods(self):
         smith = Person(given_name=u"Zöe")
-
         researcher = Researcher(person=smith)
+        force_unicode(researcher)
+        self.assertEquals(force_unicode(researcher), researcher.__unicode__())
+        smith.save()
+        smith.delete()
 
-        
-        test = researcher.__unicode__()
- 
-        force_unicode(test)
-        
-        unicode(researcher)
+class PluginTests(TestCase):
+    def setUp(self):
+        self.school = Entity(
+            name="School of Medicine", 
+            slug="medicine",
+            )
+        self.school.save()
+        self.placeholder = Placeholder(slot=u"some_slot")
+        self.placeholder.save()
+        self.plugin = add_plugin(
+            self.placeholder, 
+            u"CMSPublicationsPlugin", 
+            u"en",
+            entity=self.school
+        )
+        self.plugin.save()
+
+    def test_kind_none(self):
+        instance = self.plugin.get_plugin_instance()[1]
+        self.plugin.type = None
+        self.plugin.view = "current"
+        self.plugin.favourites_only = False
+        instance.get_items(self.plugin)
